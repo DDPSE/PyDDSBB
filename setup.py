cdc92951cda4b3ad967c08cc72975436f5790131
jyzhai <37200303+jyzhai@users.noreply.github.com>
2021-04-19 19:20:01 -0400

    set up file

---
+++ setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr 18 18:44:11 2021
+
+@author: JianyuanZhai
+"""
+
+from setuptools import setup
+
+setup(
+      name = 'PyDDSBB',
+      version = '0.1.1',
+      description = 'Data-driven Spatial Branch-and-bound Algorithm for Python',
+      author = ['Jianyuan Zhai', 'Fani Boukouvala'],
+      author_email = ['zhaijianyuan@gmail.com', 'fani.boukouvala@chbe.gatech.edu'],
+      license = 'MIT',
+      packages = ['PyDDSBB'],
+      install_requires = ['numpy','pyomo','scikit-learn', 'glpk', 'ipopt']
+      )

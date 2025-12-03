b3db9df39b09f7732e34df754bf68c215f19bb5b
Suryateja-Ravutla <sravutla3@gatech.edu>
2025-12-02 20:48:39 -0500

    Included new surrogate options for multifidelity

--- PyDDSBB/_machine_learning.py
+++ PyDDSBB/_machine_learning.py
@@ -2,8 +2,8 @@
 # -*- coding: utf-8 -*-
 
 """
-@Created on Tue Jul  7 13:32:23 2020
-@Updated on Tue Jul 08 01:15:48 2025
+@Created on Tue Jul 07 13:32:23 2020
+@Updated on Tue Dec 02 18:15:48 2025
 @authors: JianyuanZhai, Suryateja Ravutla
 """
 
@@ -11,6 +11,7 @@ import time
 import numpy as np
 
 from sklearn.svm import SVR
+from sklearn.neural_network import MLPRegressor
 from sklearn.model_selection import train_test_split
 
 class MachineLearning:
@@ -18,11 +19,14 @@ class MachineLearning:
     Base class for all machine learning model
     """
     def __init__(self, model = 'SVR'):
-        self._model_type = 'SVR'
+        self._model_type = model
     def _predict(self, X, y, multifidelity):
         return self.model.predict(X)
 
 class LocalSVR(MachineLearning):
+    """
+    Support Vector Regression surrogate using sklearn's SVR.
+    """
     def __init__(self, eps = 0.01, var_select = True):
         """
         Inputs
@@ -67,3 +71,67 @@ class LocalSVR(MachineLearning):
         crit = np.sum(np.sum(norm*coef_product*norm2.T, 1),1)       
         self.time_var_select += time.time() - time_start
         return crit
+    
+class NN(MachineLearning):
+    """
+    Neural-network-based surrogate using sklearn's MLPRegressor.
+    """
+    def __init__(self,
+                 hidden_layer_sizes = (15, 20, 15),
+                 activation = 'tanh',
+                 solver = 'lbfgs',
+                 learning_rate_init = 0.01,
+                 random_state = 26,
+                 max_iter = 100000):
+        """
+        Inputs
+        ------
+        hidden_layer_sizes: tuple
+             Sizes of hidden layers.
+        activation: str
+             Activation function ('identity', 'logistic', 'tanh', 'relu').
+        solver: str
+             Optimizer ('lbfgs', 'sgd', 'adam').
+        learning_rate_init: float
+             Initial learning rate for weight updates (used with 'sgd' or 'adam').
+        random_state: int
+             Random seed for reproducibility.
+        max_iter: int
+             Maximum number of training iterations.
+        """
+        super().__init__('MLPRegressor')
+
+        self.time_training = 0.
+
+        self.hidden_layer_sizes = hidden_layer_sizes
+        self.activation = activation
+        self.solver = solver
+        self.learning_rate_init = learning_rate_init
+        self.random_state = random_state
+        self.max_iter = max_iter
+
+    def _train(self, X, Y):
+        """
+        Inputs
+        ------
+        X: ndarray of shape (n_samples, n_variables)
+           Input data.
+        Y: ndarray of shape (n_samples,) or (n_samples, 1)
+           Output data.
+        """
+        time_start = time.time()
+        self.dim = X.shape[1]
+
+        nn = MLPRegressor(
+            hidden_layer_sizes = self.hidden_layer_sizes,
+            activation         = self.activation,
+            solver             = self.solver,
+            learning_rate_init = self.learning_rate_init,
+            random_state       = self.random_state,
+            max_iter           = self.max_iter
+        )
+
+        self.model = nn.fit(X, Y)
+        self.time_training += time.time() - time_start
+
+        
